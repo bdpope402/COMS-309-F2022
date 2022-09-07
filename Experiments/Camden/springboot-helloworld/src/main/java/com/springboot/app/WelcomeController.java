035cfde468eb38f0e29e822f63722fae3cdbf33d
Softchair <24703877+Softchair@users.noreply.github.com>
2022-09-07 13:23:12 -0500

    added new page with /welcome

--- Experiments/Camden/springboot-helloworld/src/main/java/com/springboot/app/WelcomeController.java
+++ Experiments/Camden/springboot-helloworld/src/main/java/com/springboot/app/WelcomeController.java
@@ -1,5 +1,13 @@
 package com.springboot.app;
 
-//@Restcontroller
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome!!";
+    }
 }
