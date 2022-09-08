650990e10a5186f4eb6095c5ffbf620a794280f3
Softchair <24703877+Softchair@users.noreply.github.com>
2022-09-08 12:02:40 -0500

    finihsed put and delete mappings

--- Experiments/Camden/springboot-helloworld/src/main/java/com/springboot/app/user/UserController.java
+++ Experiments/Camden/springboot-helloworld/src/main/java/com/springboot/app/user/UserController.java
@@ -7,41 +7,6 @@ import java.util.Map;
 
 @RestController
 public class UserController {
-
-//    //Hashmap of all employees
-//    HashMap<String, User> employeeList = new HashMap<>();
-//
-//    //Returns a list of all employees using list operation
-//    @GetMapping("/employee")
-//    public @ResponseBody HashMap<String, User> getAllEmployees() {
-//        return employeeList;
-//    }
-//
-//
-//    @PostMapping("/employee")
-//    public @ResponseBody String createEmployee(@RequestBody User newEmployee) {
-//        employeeList.put(newEmployee.getName(), newEmployee);
-//        return "New employee " + newEmployee.getName() + " entered";
-//    }
-//
-//    @GetMapping("/employee/{name}")
-//    public @ResponseBody User getEmployee(@PathVariable String name) {
-//        User e = employeeList.get(name);
-//        return e;
-//    }
-//
-//    // THIS IS THE UPDATE OPERATION
-//    // We extract the person from the HashMap and modify it.
-//    // Springboot automatically converts the Person to JSON format
-//    // Springboot gets the PATHVARIABLE from the URL
-//    // Here we are returning what we sent to the method
-//    // in this case because of @ResponseBody
-//    // Note: To UPDATE we use PUT method
-//    @PutMapping("/employee/{name}")
-//    public @ResponseBody User updateEmployee(@PathVariable String name, @RequestBody User e) {
-//        employeeList.replace(name, e);
-//        return employeeList.get(name);
-//    }
 //
 //    // THIS IS THE DELETE OPERATION
 //    // Springboot gets the PATHVARIABLE from the URL
@@ -75,13 +40,15 @@ public class UserController {
         return "New user " + newUser.getName() + " Added as " + newUser.getRole();
     }
 
-    @DeleteMapping()
-    public String deleteUser() {
-        return "HTTP Delete request was sent";
+    @PutMapping("/users/{name}")
+    public String updateUser(@RequestParam String oldName, @RequestBody User newUser) {
+        userList.replace(oldName, newUser);
+        return "Updated " + oldName;
     }
 
-    @PutMapping()
-    public String updateUser() {
-        return "HTTP Put request was sent";
+    @DeleteMapping("/users/{name}")
+    public String deleteUser(@RequestParam String name) {
+        userList.remove(name);
+        return "User " + name + " was deleted";
     }
 }
