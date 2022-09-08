7cbf4ecac58f70a39878d53a53c1eee53f8591fd
Michael Less <mless@iastate.edu>
2022-09-07 19:32:35 -0500

    Experiment 1

--- Experiments/mless/Experiment_1/app/src/main/java/com/example/experiment_1/MainActivity.java
+++ Experiments/mless/Experiment_1/app/src/main/java/com/example/experiment_1/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.experiment_1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.widget.EditText;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
 }
