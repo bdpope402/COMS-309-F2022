f877b101d2d9ce913c5c0dfa30b5c15902e03572
Michael Less <mless@iastate.edu>
2022-09-21 17:55:04 -0500

    Account details page page

---
+++ Frontend/app/src/main/java/com/example/app/AccountDetails.java
@@ -0,0 +1,26 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AccountDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_details);
+
+        Button back = findViewById((R.id.back_menu));
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(), MainActivity.class));
+            }
+        });
+    }
+}
