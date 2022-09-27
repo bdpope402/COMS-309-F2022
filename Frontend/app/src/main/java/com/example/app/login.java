66116aeb48591c01efb7b2fc50cd1cc11e938b41
Zeksteth <zeksteth@gmail.com>
2022-09-26 22:09:10 -0500

    messing with volley

--- Frontend/app/src/main/java/com/example/app/login.java
+++ Frontend/app/src/main/java/com/example/app/login.java
@@ -1,14 +1,32 @@
 package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.example.app.utils.Const;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request.Method;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import org.json.JSONArray;
 
 public class login extends AppCompatActivity {
+
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = (button)
     }
 }
