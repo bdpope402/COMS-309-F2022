96bc085052fd0c0ffe42c8534a29a27d5f75eec0
Zeksteth <zeksteth@gmail.com>
2022-09-26 22:39:10 -0500

    failed to try to get volley to work

--- Frontend/app/src/main/java/com/example/app/login.java
+++ Frontend/app/src/main/java/com/example/app/login.java
@@ -1,13 +1,19 @@
 package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
 
+import com.example.app.app.AppController;
 import com.example.app.utils.Const;
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request.Method;
@@ -17,16 +23,73 @@ import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
-public class login extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
+
+public class login extends Activity implements OnClickListener {
 
     private Button login;
+    private TextView msgResponse;
+    private String TAG = login.class.getSimpleName();
+    private String tag_json_obj = "jobj_req";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        login = (button)
+        login = (Button) findViewById(R.id.login);
+        msgResponse = (TextView) findViewById(R.id.msgResponse);
+
+        login.setOnClickListener(this);
+    }
+
+    private void makeJsonObjReq() {
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Method.GET,
+                Const.URL_JSON_OBJECT, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, response.toString());
+                        msgResponse.setText(response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+            }
+        }) {
+
+            /**
+             * Passing some request headers
+             */
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("username, "Androidhive");
+                params.put("email", "abc@androidhive.info");
+                params.put("pass", "password123");
+
+                return params;
+            }
+        };
+        AppController.getInstance().addToRequestQueue(jsonObjReq,
+                tag_json_obj);
+    }
+
+    @Override
+    public void onClick(View v) {
+        makeJsonObjReq();
     }
 }
