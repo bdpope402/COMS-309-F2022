bb54b99585e3727312ae4132c1ebfc9b041e391e
bdpope <bryan.pope.d@gmail.com>
2022-10-04 18:22:43 -0500

    misc updates

--- Backend/SpringBackend/src/main/java/com/infoFootball/SpringBackend/SpringBackendApplication.java
+++ Backend/SpringBackend/src/main/java/com/infoFootball/SpringBackend/SpringBackendApplication.java
@@ -1,11 +1,16 @@
 package com.infoFootball.SpringBackend;
 
-import User.UserController;
+//import SpringBackend.User.UserController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.hibernate.*;
+
 
 @SpringBootApplication
 @RestController
@@ -19,6 +24,11 @@ public class SpringBackendApplication {
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
+//	@Bean(name=entityManagerFactory)
+//	public LocalSessionFactoryBean sessionFactory() {
+//		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+//		return sessionFactory;
+//	}
 
 
 }
