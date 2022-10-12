8b3c2f8dc7a59ece353b6d060eceb83f25931262
Zeksteth <zeksteth@gmail.com>
2022-10-12 10:32:46 -0500

    connected the admin page to the grant admin page

--- Frontend/app/src/main/java/com/example/app/admin_page.java
+++ Frontend/app/src/main/java/com/example/app/admin_page.java
@@ -1,8 +1,11 @@
 package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.app.adminfeatures.grant_admin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class admin_page extends AppCompatActivity {
@@ -13,5 +16,15 @@ public class admin_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_page);
+
+        grant_admin = (Button) findViewById(R.id.grant_admin);
+
+        grant_admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(admin_page.this, grant_admin.class);
+                startActivity(intent);
+            }
+        });
     }
 }
