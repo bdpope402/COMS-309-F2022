d1e68e703d74bc09927f951508b18d3ee1920b99
bdpope <bryan.pope.d@gmail.com>
2022-10-11 22:04:15 -0500

    created player class

---
+++ Backend/SpringBackend/src/main/java/com/infoFootball/SpringBackend/Player/Player.java
@@ -0,0 +1,52 @@
+package com.infoFootball.SpringBackend.Player;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * @author bdpope
+ * This class is to create a player object. This contains a unique player IDnum, their first and last name,
+ * the player's number. We use this as the ID to the stats table
+ */
+@Entity
+public class Player {
+
+    @Id @GeneratedValue
+    private int IdNum;
+    private String firstName;
+    private String lastName;
+    private String playerNum;
+
+    public Player(String firstName, String lastName, String playerNum){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.playerNum = playerNum;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+    public void setlastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public String getplayerNum(){
+        return playerNum;
+    }
+    public void setplayerNum(String playerNum){
+        this.playerNum = playerNum;
+    }
+
+    public int getIdNum(){
+        return IdNum;
+    }
+
+}
