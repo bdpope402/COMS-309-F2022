6ec6b7162ef1761c08cfd633be0376940bdd1207
Zeksteth <zeksteth@gmail.com>
2022-10-11 19:27:57 -0500

    Added admin profile page and grant_admin page

---
+++ Frontend/app/src/main/java/com/example/app/admin_page.java
@@ -0,0 +1,17 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class admin_page extends AppCompatActivity {
+
+    Button grant_admin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_page);
+    }
+}
