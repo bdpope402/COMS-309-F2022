d1ea9caf897740183e56397a9868929b4e5e8207
Zeksteth <zeksteth@gmail.com>
2022-10-25 16:09:11 -0500

    Figured out how to make multiple buttons with constraints programmatically

--- Frontend/app/src/main/java/com/example/app/concessions.java
+++ Frontend/app/src/main/java/com/example/app/concessions.java
@@ -18,29 +18,49 @@ import android.widget.LinearLayout;
 
 import com.example.app.databinding.ActivityConcessionsBinding;
 
+import java.util.ArrayList;
+
 public class concessions extends AppCompatActivity {
+    ArrayList<Button> buttons;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_concessions);
+        buttons = new ArrayList<>();
 
         ConstraintLayout lin = findViewById(R.id.concessions);
         int y = 100;
-        int x = 100;
+        int x = 400;
+        int i;
 
-        Button n = new Button(this);
-        n.setId(1);
-        lin.addView(n);
+        for (i = 0; i < 4; i++) {
+            Button n = new Button(this);
+            n.setId(i + 1);
+            n.setTag(i + 1);
+            n.setText("hi");
+            buttons.add(n);
+        }
+
+        for (i = 0; i < 4; i++) {
+            lin.addView(buttons.get(i));
+        }
+
         ConstraintSet constraint = new ConstraintSet();
-        constraint.clone(lin);
-        ConstraintSet.connect(R.id.button1)
-        n.setText("hi");
-        //n.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        n.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                System.out.print("hi");
-            }
-        });
+        for (i = 0; i < 4; i++) {
+            constraint.clone(lin);
+            constraint.connect(buttons.get(i).getId(), ConstraintSet.RIGHT, R.id.concessions, ConstraintSet.RIGHT, y);
+            constraint.connect(buttons.get(i).getId(), ConstraintSet.TOP, R.id.concessions, ConstraintSet.TOP, (x + (100 * i)));
+            constraint.applyTo(lin);
+        }
+
+//        n.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                System.out.print("hi");
+//            }
+//        });
+//
+//        lin.addView(n);
     }
 }
