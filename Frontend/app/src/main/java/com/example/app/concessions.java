6823980931cf8b3b9feb8357f77a2d8fd7aa78b0
Zeksteth <zeksteth@gmail.com>
2022-10-25 15:31:44 -0500

    messing with dynamically programmed buttons

--- Frontend/app/src/main/java/com/example/app/concessions.java
+++ Frontend/app/src/main/java/com/example/app/concessions.java
@@ -1,40 +1,45 @@
 package com.example.app;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.example.app.databinding.ActivityConcessionsBinding;
 
 public class concessions extends AppCompatActivity {
-
-    private ActivityConcessionsBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_concessions);
 
-        binding = ActivityConcessionsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        ConstraintLayout lin = findViewById(R.id.concessions);
+        int y = 100;
+        int x = 100;
 
-        Toolbar toolbar = binding.toolbar;
-        setSupportActionBar(toolbar);
-        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
-        toolBarLayout.setTitle(getTitle());
-
-        FloatingActionButton fab = binding.fab;
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button n = new Button(this);
+        n.setId(1);
+        lin.addView(n);
+        ConstraintSet constraint = new ConstraintSet();
+        constraint.clone(lin);
+        ConstraintSet.connect(R.id.button1)
+        n.setText("hi");
+        //n.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        n.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                System.out.print("hi");
             }
         });
     }
