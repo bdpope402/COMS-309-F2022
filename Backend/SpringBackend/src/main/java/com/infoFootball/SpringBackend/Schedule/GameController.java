5a8f04a6007a46bdeba17ef9e1c14e8af2a104c9
bdpope <bryan.pope.d@gmail.com>
2022-11-16 10:14:49 -0600

    More work on gameController

--- Backend/SpringBackend/src/main/java/com/infoFootball/SpringBackend/Schedule/GameController.java
+++ Backend/SpringBackend/src/main/java/com/infoFootball/SpringBackend/Schedule/GameController.java
@@ -2,10 +2,7 @@ package com.infoFootball.SpringBackend.Schedule;
 
 import com.infoFootball.SpringBackend.Player.Player;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.infoFootball.SpringBackend.Team.*;
 
 import javax.persistence.FetchType;
@@ -22,14 +19,31 @@ public class GameController {
     final private String failure = "{\"message\":\"failure\"}";
 
     @GetMapping(path = "/games")
-    List<Game> getAllGames(){
+    List<Game> getAllGames() {
         return gameRespository.findAll();
     }
 
-    @PostMapping(path="/games/new")
+    @GetMapping(path = "/games/{awayTeam}")
+    Game getGameByAwayTeam(@PathVariable Team awayTeam) {
+        return gameRespository.findByAwayTeam(awayTeam);
+    }
+
+    @PutMapping(path = "/games/{awayTeam}")
+    Game updateGame(@PathVariable Team awayTeam, @RequestBody Game newGame) {
+        Game game = gameRespository.findByAwayTeam(awayTeam);
+        if (game.equals(null)) {
+            return null;
+        }
+        gameRespository.deleteByAwayTeam(awayTeam);
+        gameRespository.save(newGame);
+        return gameRespository.findByAwayTeam(newGame.getAwayTeam());
+    }
+
+    @PostMapping(path = "/games/new")
     public String newGame(@RequestBody Game newGame) {
         gameRespository.save(newGame);
         return "Success";
     }
-
 }
+
+
