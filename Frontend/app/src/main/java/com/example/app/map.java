0ee49dc44d686eaebe35631e029f7624169609f5
Michael Less <mless@iastate.edu>
2022-12-06 16:50:16 -0600

    Comments for map code

--- Frontend/app/src/main/java/com/example/app/map.java
+++ Frontend/app/src/main/java/com/example/app/map.java
@@ -1,3 +1,9 @@
+/**
+ * @author Michael Less
+ * This screen implements the google map API, allowing the user to view the map
+ * and create their own pins with inputted Lat/Lng
+ */
+
 package com.example.app;
 
 import androidx.fragment.app.FragmentActivity;
@@ -17,6 +23,11 @@ public class map extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
     private ActivityMapBinding binding;
 
+    /**
+     * Creates the screen based off of the .xml file associated with the activity and adds logic for
+     * things like button presses and other functions.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +49,7 @@ public class map extends FragmentActivity implements OnMapReadyCallback {
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
+     * @param googleMap 
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
