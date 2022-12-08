507c757c4145fbcac8326feba620a7556bddc412
bdpope <bryan.pope.d@gmail.com>
2022-12-08 10:36:47 -0600

    bad commit

--- Backend/SpringBackend/src/main/java/com/infoFootball/SpringBackend/SpringBackendApplication.java
+++ Backend/SpringBackend/src/main/java/com/infoFootball/SpringBackend/SpringBackendApplication.java
@@ -3,6 +3,7 @@ package com.infoFootball.SpringBackend;
 import com.infoFootball.SpringBackend.Chat.ConfigWebSocket;
 import com.infoFootball.SpringBackend.Chat.FriendRelationController;
 import com.infoFootball.SpringBackend.Chat.WebSocket;
+import com.infoFootball.SpringBackend.GlobalChat.WebSocketServer;
 import com.infoFootball.SpringBackend.Concessions.FoodItem.FoodItemController;
 import com.infoFootball.SpringBackend.Concessions.FoodMenu.FoodMenuController;
 import com.infoFootball.SpringBackend.Concessions.FoodMenu.FoodMenuService;
@@ -20,7 +21,7 @@ import org.springframework.context.annotation.Import;
 @SpringBootConfiguration(proxyBeanMethods = false)
 @EnableAutoConfiguration
 @Import({LoginController.class, RegisterController.class, UserController.class, PinController.class, VendorController.class, FoodMenuController.class,
-		FoodMenuService.class, FoodItemController.class, PlayerController.class, WebSocket.class, ConfigWebSocket.class, FriendRelationController.class})
+		FoodMenuService.class, FoodItemController.class, PlayerController.class, WebSocket.class, WebSocketServer.class, ConfigWebSocket.class, FriendRelationController.class})
 public class SpringBackendApplication {
 
 	public static void main(String[] args) {
