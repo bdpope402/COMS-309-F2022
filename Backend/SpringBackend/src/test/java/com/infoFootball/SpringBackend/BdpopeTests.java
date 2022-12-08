45e2111dd944dc06bb65abad8215231135d3aa62
bdpope <bryan.pope.d@gmail.com>
2022-12-07 19:07:17 -0600

    Added bdpope tests

--- Backend/SpringBackend/src/test/java/com/infoFootball/SpringBackend/BdpopeTests.java
+++ Backend/SpringBackend/src/test/java/com/infoFootball/SpringBackend/BdpopeTests.java
@@ -1,13 +1,17 @@
 package com.infoFootball.SpringBackend;
 
+import com.infoFootball.SpringBackend.Player.Player;
 import com.infoFootball.SpringBackend.Player.PlayerRespository;
 import com.infoFootball.SpringBackend.Register.RegisterController;
 import com.infoFootball.SpringBackend.User.User;
 import com.infoFootball.SpringBackend.User.UserRepository;
 import io.restassured.response.Response;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.aspectj.lang.annotation.Before;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,5 +28,35 @@ public class BdpopeTests {
     int port;
 
     @Autowired
-    PlayerRespository userRepository;
+    PlayerRespository playerRepository;
+
+    @Before
+    public void Setup(){
+        RestAssured.port = port;
+        RestAssured.baseURI = "http://localhost";
+    }
+
+    @Test
+    public void testNewPlayer() throws Exception {
+        JSONObject jsonPlayer = new JSONObject();
+        jsonPlayer.put("firstName", "testplayer");
+        jsonPlayer.put("lastName", "Doe");
+        jsonPlayer.put("playerNum", 420);
+
+        Player newPlayer = new Player("testPlayer", "Doe", "12");
+
+        Response response = RestAssured.given();
+                .header("Content-Type", "application/json")
+                .body(jsonPlayer.toString()) //Send in json USER
+                .when().request("POST", "/player/new"); //The url and request type
+
+        int statusCode = response.getStatusCode();
+        assertEquals(200, statusCode);
+
+        //Check that it returns a success
+        String returnString = response.getBody().asString();
+        assertEquals("Success", returnString);
+    }
+
+
 }
